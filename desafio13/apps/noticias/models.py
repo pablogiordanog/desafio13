183b2441df8f64b30f1ab536e4b722e95b0e8b26
Pablo Giordano <pablogiordano@mbp-de-pablo.home>
2023-07-19 17:51:02 -0300

    Agrege opcion en index

--- desafio13/apps/noticias/models.py
+++ desafio13/apps/noticias/models.py
@@ -14,7 +14,7 @@ class Noticia(models.Model):
     autor = models.CharField(max_length=20, null=False)
     descripcion = models.TextField()
     published = models.DateTimeField(default=timezone.now)
-    imagen = models.ImageField(null=True, blank=True, upload_to='libros', default='libros/libro_default.png')
+    imagen = models.ImageField(null=True, blank=True, upload_to='noticias', default='noticias/notice_default.png')
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
