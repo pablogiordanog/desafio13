e31cc485f2c577842ccb44e9eebb8930d9df4535
Pablo Giordano <pablogiordano@mbp-de-pablo.home>
2023-07-20 20:43:23 -0300

    Se Integro Acerca de

--- desafio13/desafio13/urls.py
+++ desafio13/desafio13/urls.py
@@ -4,13 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from .views import index
+from .views import index, acerca_de
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index, name='index'),
     path('noticias/', include('apps.noticias.urls')),
     path('categorias/', include('apps.categorias.urls')),
+    path('acerca_de/', acerca_de, name='acerca_de'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
--- desafio13/desafio13/views.py
+++ desafio13/desafio13/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     template_name = 'index.html'
     return render(request,template_name)
+
+def acerca_de(request):
+    template_name = 'acerca_de.html'
+    return render(request,template_name)
